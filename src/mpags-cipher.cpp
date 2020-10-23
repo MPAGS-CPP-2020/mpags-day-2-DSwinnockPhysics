02469777ba20d66523415d89e1674947cded6e35
root <Daniel.Swinnock@warwick.ac.uk>
2020-10-23 12:13:26 +0000

    Moved TransformChar and ProcessCommandLine into separate files, Added CMake stuff.

--- src/mpags-cipher.cpp
+++ src/mpags-cipher.cpp
@@ -2,133 +2,12 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include "TransformChar.hpp"
+#include "ProcessCommandLine.hpp"
 
 // For std::isalpha and std::isupper
 #include <cctype>
 
-std::string transformChar ( const char inputChar )
-{
-  /* Turn alpha character uppercase or convert number to word
-
-  const char inputChar: The character to be processed
-
-  return: String containing uppercase character or word
-  */
-
-  std::string inputText {""};
-
-    // Uppercase alphabetic characters
-  if (std::isalpha(inputChar)) {
-    inputText += std::toupper(inputChar);
-    //continue;
-    return inputText;
-  }
-
-  // Transliterate digits to English words
-  switch (inputChar) {
-    case '0':
-	inputText += "ZERO";
-	break;
-      case '1':
-	inputText += "ONE";
-	break;
-      case '2':
-	inputText += "TWO";
-	break;
-      case '3':
-	inputText += "THREE";
-	break;
-      case '4':
-	inputText += "FOUR";
-	break;
-      case '5':
-	inputText += "FIVE";
-	break;
-      case '6':
-	inputText += "SIX";
-	break;
-      case '7':
-	inputText += "SEVEN";
-	break;
-      case '8':
-	inputText += "EIGHT";
-	break;
-      case '9':
-	inputText += "NINE";
-	break;
-  }
-
-  // If the character isn't alphabetic or numeric, DONT add it.
-  // Our ciphers can only operate on alphabetic characters.
-
-  return inputText;
-}
-
-
-bool processCommandLine(  const std::vector<std::string>& cmdLineArgs,
-  bool& helpRequested,
-  bool& versionRequested,
-  std::string& inputFile,
-  std::string& outputFile ) 
-  {
-  // Add a typedef that assigns another name for the given type for clarity
-  typedef std::vector<std::string>::size_type size_type;
-  const size_type nCmdLineArgs {cmdLineArgs.size()};
-
-  // Process command line arguments - ignore zeroth element, as we know this to
-  // be the program name and don't need to worry about it
-  for (size_type i {1}; i < nCmdLineArgs; ++i) {
-
-    if (cmdLineArgs[i] == "-h" || cmdLineArgs[i] == "--help") {
-      helpRequested = true;
-    }
-    else if (cmdLineArgs[i] == "--version") {
-      versionRequested = true;
-    }
-    else if (cmdLineArgs[i] == "-i") {
-      // Handle input file option
-      // Next element is filename unless -i is the last argument
-      if (i == nCmdLineArgs-1) {
-	std::cerr << "[error] -i requires a filename argument" << std::endl;
-  // Return false to indicate an error occurred
-	return false;
-      }
-      else {
-	// Got filename, so assign value and advance past it
-	inputFile = cmdLineArgs[i+1];
-	++i;
-      }
-    }
-    else if (cmdLineArgs[i] == "-o") {
-      // Handle output file option
-      // Next element is filename unless -o is the last argument
-      if (i == nCmdLineArgs-1) {
-	std::cerr << "[error] -o requires a filename argument" << std::endl;
-	// Return false to indicate an error occurred
-	return false;
-      }
-      else {
-	// Got filename, so assign value and advance past it
-	outputFile = cmdLineArgs[i+1];
-	++i;
-      }
-    }
-    else {
-      // Have an unknown flag to output error message and return false
-      // exit status to indicate failure
-      std::cerr << "[error] unknown argument '" << cmdLineArgs[i] << "'\n";
-      return false;
-    }
-  }
-
-
-
-  return true;
-
-}
-
-
-//////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 // Main function of the mpags-cipher program
 int main(int argc, char* argv[])
@@ -210,3 +89,4 @@ int main(int argc, char* argv[])
   // and for consistency with other functions
   return 0;
 }
+
